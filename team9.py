831ca8c9d78ed22640a3da5e7e8441ba637f8c1d
braxtoncornell <braxton.cornell.3163@gmail.com>
2017-03-08 09:50:16 -0500

    Add files via upload

---
+++ team9.py
@@ -0,0 +1,20 @@
+import random
+
+team_name = 'El Chapo 2'
+strategy_name = 'El Chapo 2.0' 
+strategy_description = 'b if they betray more often'
+
+def move(my_history, their_history, my_score, their_score):
+    ### my_history, their_history are strings.
+    ### my_score, their_score are ints.
+    if b >= c in their_history:
+        return b
+    elif b == 0 and c == 0 in their_history:
+        random.randint(1,2)
+        if random.randint == 1:
+            return b
+        else:
+            return c      
+    else:
+        return c
+
